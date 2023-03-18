aee5fc5842038860d22862dd8e818b2d4d8d6b56
Alexandru Tugui <loveamusementgeo@gmail.com>
2023-03-19 00:20:15 +0200

    Change some design

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ I am a Web Design Freelancer from <strong>Craiova</strong>, specialized in creat
   },
 ];
 
+
 const educationData = [
   {
     degree: 'Master - Information Systems for e-Business',
@@ -242,8 +243,8 @@ const avatarurl = 'https://alextag.ro/wp-content/uploads/2021/02/60251212_452404
 
 function App() {
   return (
-    <div className="bg-gray-100 min-h-screen">
-      <div className="container mx-auto max-w-screen-md p-8">
+    <div class="container newcointainersize mx-auto max-w-screen-md px-4 mx-1.50 py-2 flex justify-center md:justify-between">
+    <div class="bg-gray-100 min-h-screen">
       <header class="bg-white shadow-md">
     <div class="container newcointainersize mx-auto px-4 mx-1.50 py-2 flex justify-center md:justify-between">
       <button
@@ -277,7 +278,8 @@ function App() {
 
     </div>
   </header>
-  <div class="container newcointainersize-content mx-auto px-4 py-8 flex">
+
+  <div class="container mx-auto px-4 py-8 px-4 flex">
   <div>
     <h1 class="text-4xl font-bold mb-4">Alex Tag</h1>
     <p class="text-gray-700">Web Developer</p>
@@ -293,15 +295,14 @@ function App() {
 </div>
   
 </div>
-<div id="about" class="bg-blue-200 text-blue-900 newcointainersize-content p-4 md:p-8 rounded shadow-md mx-auto"></div>
-  <div id="experience" class="hidden bg-blue-200 text-blue-900 newcointainersize-content p-4 md:p-8 rounded shadow-md mx-auto"></div>
-  <div id="education" class="hidden bg-blue-200 text-blue-900 newcointainersize-content p-4 md:p-8 rounded shadow-md mx-auto"></div>
-  <div id="skills" class="hidden bg-blue-200 text-blue-900 newcointainersize-content p-4 md:p-8 rounded shadow-md mx-auto"></div>
-  <div id="portofolio" class="hidden bg-blue-200 text-blue-900 newcointainersize-content p-4 md:p-8 rounded shadow-md mx-auto"></div>
+<div id="about" class="bg-blue-200 text-blue-900 mx-5 p-4 md:p-8 rounded shadow-md mx-auto"></div>
+  <div id="experience" class="hidden bg-blue-200 mx-5 text-blue-900  p-4 md:p-8 rounded shadow-md mx-auto"></div>
+  <div id="education" class="hidden bg-blue-200 mx-5 text-blue-900  p-4 md:p-8 rounded shadow-md mx-auto"></div>
+  <div id="skills" class="hidden bg-blue-200 mx-5 text-blue-900  p-4 md:p-8 rounded shadow-md mx-auto"></div>
+  <div id="portofolio" class="hidden bg-blue-200 mx-5 text-blue-900  p-4 md:p-8 rounded shadow-md mx-auto"></div>
 
       </div>
     </div>
-    
   );
 }
 
